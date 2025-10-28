18755eeb3cc6f897ae5a4534278be90ba2e99bfb
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-28 15:08:30 -0300

    feat - creating methods to handle with auth process.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,25 +1,89 @@
+import { baseURL } from "./baseURL";
+import { getAccessTokenFromLocalStorage } from "./storageForCredentials";
+
+const url = `${baseURL}/auth`;
+
 export async function signin(email, password) {
   try {
     const encodedData = btoa(`${email}:${password}`);
     const authHeader = `Basic ${encodedData}`;
-    const request = new Request("http://127.0.0.1:3000/auth/signin", {
+    const request = new Request(`${url}/signin`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: authHeader,
+        "Authorization": authHeader
       },
-      credentials: "include"
+      credentials: "include",
     });
 
     const response = await fetch(request);
 
     if (!response.ok) {
-      throw new Error("Failed to signin to the system.");
+      throw new Error("Signin response is not valid.");
     }
 
     return await response.json();
   } catch (error) {
-    console.log(`Failed to signin to the system. Error: ${error}`);
-    throw new Error("Failed to signin to the system.");
+    throw new Error("Failed to signin from the system.");
+  }
+};
+
+export async function signup(username, email, password) {
+  try {
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    const request = new Request(`${url}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: data,
+    });
+
+    const response = await fetch(request);
+
+    if (!response.ok) {
+      throw new Error("Signup response is not valid.");
+    }
+
+    return await response.json()
+  } catch {
+    throw new Error("Failed to signup from the system.");
+  }
+};
+
+export async function signout() {
+  try {
+    const accessToken = getAccessTokenFromLocalStorage();
+
+    if (accessToken) {
+      const authHeader = `Bearer ${accessToken}`;
+
+      const request = new Request(`${url}/signout`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": authHeader
+        },
+        credentials: "include"
+      });
+
+      const response = await fetch(request);
+
+      if (!response.ok) {
+        throw new Error("Failed to signout from the system.");
+      }
+
+      return true
+    } else {
+      throw new Error("Missing Data to signout from the system.")
+    }
+  } catch {
+    throw new Error("Failed to signout from the system.");
   }
 }
