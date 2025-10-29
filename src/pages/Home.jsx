f01224283b653644d62fb7535fce8ce231ea4240
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-29 14:59:29 -0300

    cleanup - removing unused imported object.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 import { Card } from "../components/Card"
 import { Select } from "../components/Select"
-import { Plus, Edit, Trash, LogOut } from "lucide-react"
+import { Plus, Edit, Trash } from "lucide-react"
 import { ProfileSection } from "../components/ProfileSection"
 import { useState } from "react"
 import "../index.css";
